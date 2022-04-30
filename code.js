3279c0def86949dedcccf26499b38ad12121a1e9
Nate Mac <sportnate@gmail.com>
2022-04-30 09:30:32 -0400

    Excerise from internetingishard.com/html-and-css/hello-css/

---
+++ code.js
@@ -0,0 +1,25 @@
+
+
+// // My Solution
+// const add = function (a, b) {
+//     return a + b;
+//   }
+//   const multiply = function (a, b) {
+//     let result = 0;
+//     // TODO: Write a for loop which adds the "a" number to itself "b" times.
+//     return result;
+//   }
+//   // My Unit Test
+//   const testMultiply = function () {
+//     // start testing a happy path
+//     let result = multiply(2, 3);
+//     console.assert(result === 6, JSON.stringify({
+//       "test": 'multiply(2, 3)',
+//       "expected": 6,
+//       "result": result 
+//     }));
+//   }
+//   testMultiply();
+
+
+  
---
+++ readMe.md
@@ -0,0 +1,3 @@
+
+
+The following files are for daily practice with HTML, CSS, and Javascript. As I (Nate McReynolds) prepare for a career in Front End Development, I will be logging my lessons and journey on github.
